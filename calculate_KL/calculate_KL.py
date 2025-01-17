ed0d0e7b3effcc59edd82af59db759583d86cfa8
Storm Colloms <2297403c@wiay.astro.gla.ac.uk>
2025-01-17 13:00:19 +0000

    updated calc KLs to run and save flow and KDE KLs

--- calculate_KL/calculate_KL.py
+++ calculate_KL/calculate_KL.py
@@ -4,7 +4,7 @@ import argparse
 from tqdm import tqdm
 
 import sys
-sys.path.append('../../')
+sys.path.append('../')
 from populations.bbh_models import read_hdf5
 import populations.bbh_models as read_models
 from populations import gw_obs
@@ -12,6 +12,7 @@ from populations import gw_obs
 #arguments
 argp = argparse.ArgumentParser()
 argp.add_argument("--channel-label", type=str, nargs="+", default="CE", help="")
+argp.add_argument("--Neventsamps", type=int, default="11058", help="")
 argp.add_argument("--no-samps", type=int, default="1000", help="")
 argp.add_argument("--flow-path", type=str, default=None, help="Directory from where to load flow models. Default=None.")
 argp.add_argument("--gw-path", type=str, default=None, help="Directory from where to load GW observations. Default=None.")
@@ -29,7 +30,7 @@ gw_path = args.gw_path
 models_path ='/data/wiay/2297403c/models_reduced.hdf5'
 np.random.seed(12)
 observations, obsdata, p_theta, events = gw_obs.generate_observations(params, gw_path, \
-                                        no_samples, 'posteriors', prior='p_theta_jcb')
+                                        args.Neventsamps, 'posteriors', prior='p_theta_jcb')
 
 for channel_label in channels:
     #read in popsynth models
@@ -41,11 +42,11 @@ for channel_label in channels:
         weights_dict[key]= popsynth_outputs[key]['weight']
 
     #initialise flow and KDE models
-    model_names, flow = read_models.get_models(models_path, [channel_label], params, use_flows=True, device='cuda:0', no_bins=[5])
+    model_names, flow = read_models.get_models(models_path, [channel_label], params, use_flows=True, device='cuda:0')
     _, KDE = read_models.get_models(models_path, [channel_label], params, use_flows=False, device='cpu')
 
     flow_path=args.flow_path
-    flow[channel_label].load_model(flow_path, channel_label)
+    flow[channel_label].load_model(flow_path, device='cuda:0')
 
     #get model names for grabbing KDE models
     hyperparams = list(set([x.split('/', 1)[1] for x in model_names]))
@@ -65,7 +66,7 @@ for channel_label in channels:
         KDE_flow_KL = np.zeros((4,5))
 
         #cacluate KDE||flow and flow||KDE given the observed GWs
-        print('cacluating KDE||flow and flow||KDE given the observed GWs')
+        """print('cacluating KDE||flow and flow||KDE given the observed GWs')
         for chi_b_id, xb in enumerate(tqdm(chi_b)):
             for alpha_id, a in enumerate(alpha_CE):
 
@@ -76,9 +77,11 @@ for channel_label in channels:
                 flow_KDE_KL[chi_b_id,alpha_id] = entropy(p_flow, p_KDE)
                 KDE_flow_KL[chi_b_id,alpha_id] = entropy(p_KDE, p_flow)
 
-        np.save(f'data/{channel_label}_flow_KDE_KL.npy', flow_KDE_KL)
-        np.save(f'data/{channel_label}_KDE_flow_KL.npy', KDE_flow_KL)
 
+        print(f'saving KDE||flow and flow||KDE given the observed GWs for {channel_label}')
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_flow_KDE_KL.npy', flow_KDE_KL)
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_KDE_flow_KL.npy', KDE_flow_KL)
+        """
         model_samples = np.zeros((4,5,no_samples,4))
         model_weights = np.zeros((4,5,no_samples))
 
@@ -89,32 +92,33 @@ for channel_label in channels:
                 model_samples[chi_b_id,alpha_id,:,:] = np.array(models_dict[(chi_b_id,alpha_id)])[model_samples_idx]
                 model_weights[chi_b_id,alpha_id,:] = np.array(weights_dict[(chi_b_id,alpha_id)])[model_samples_idx]
 
-        #cacluate flow||models and KDE||models given the model observations
+        print(f'cacluating flow||models and KDE||models given the model observations')
         flow_KL = np.zeros((4,5))
         KDE_KL = np.zeros((4,5))
         for chi_b_id, xb in enumerate(chi_b):
             for alpha_id, a in enumerate(alpha_CE):
                 p_flow = flow[channel_label](np.reshape(model_samples[chi_b_id,alpha_id],(no_samples,1,4)), np.array([xb,np.log(a)]), 990903)
-                flow_KL[chi_b_id,alpha_id] = -np.mean(model_weights[chi_b_id,alpha_id]*p_flow)
+                flow_KL[chi_b_id,alpha_id] = -np.mean(p_flow)
                 p_kde = KDE[channel_label][submodels_dict[0][chi_b_id]][submodels_dict[1][alpha_id]](np.reshape(model_samples[chi_b_id,alpha_id],(no_samples,1,4)), 990903)
-                KDE_KL[chi_b_id,alpha_id] = -np.mean(model_weights[chi_b_id,alpha_id]*np.log(p_kde))
+                KDE_KL[chi_b_id,alpha_id] = -np.mean(np.log(p_kde))
 
-        #save flow_KL-kde_KL
-        np.save(f'data/{channel_label}_flow_KL_minus_KDE_KL.npy', flow_KL-KDE_KL)
+        print(f'saving flow_KL-kde_KL for {channel_label}')
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_flow_KL.npy', flow_KL)
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_KDE_KL.npy', KDE_KL)
     else:
         flow_KDE_KL = np.zeros((4))
         KDE_flow_KL = np.zeros((4))
         #cacluate KDE||flow and flow||KDE given the observed GWs
-        for chi_b_id, xb in enumerate(chi_b):
+        """for chi_b_id, xb in enumerate(chi_b):
             p_flow = np.exp(flow[channel_label](obsdata, np.array([chi_b[chi_b_id]]), 990903, p_theta))
             p_KDE = KDE[channel_label][submodels_dict[0][chi_b_id]](obsdata, 990903, p_theta)
 
             flow_KDE_KL[chi_b_id] = entropy(p_flow, p_KDE)
             KDE_flow_KL[chi_b_id] = entropy(p_KDE, p_flow)
 
-        np.save(f'data/{channel_label}_flow_KDE_KL.npy', flow_KDE_KL)
-        np.save(f'data/{channel_label}_KDE_flow_KL.npy', KDE_flow_KL)
-
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_flow_KDE_KL.npy', flow_KDE_KL)
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_KDE_flow_KL.npy', KDE_flow_KL)
+        """
         model_samples = np.zeros((4,no_samples,4))
         model_weights = np.zeros((4,no_samples))
 
@@ -128,8 +132,10 @@ for channel_label in channels:
         KDE_KL = np.zeros((4))
         for chi_b_id, xb in enumerate(chi_b):
             p_flow = flow[channel_label](np.reshape(model_samples[chi_b_id],(no_samples,1,4)), np.array([xb]), 990903)
-            flow_KL[chi_b_id] = - np.mean(model_weights[chi_b_id]*p_flow)
+            flow_KL[chi_b_id] = -np.mean(p_flow)
             p_kde = KDE[channel_label][submodels_dict[0][chi_b_id]](np.reshape(model_samples[chi_b_id],(no_samples,1,4)), 990903)
-            KDE_KL[chi_b_id] = -np.mean(model_weights[chi_b_id]*np.log(p_kde))
+            KDE_KL[chi_b_id] = -np.mean(np.log(p_kde))
 
-        np.save(f'data/{channel_label}_flow_KL_minus_KDE_KL.npy', flow_KL-KDE_KL)
+        print(f'saving flow_KL-kde_KL for {channel_label}')
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_flow_KL.npy', flow_KL)
+        np.save(f'/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224/data/{channel_label}_KDE_KL.npy', KDE_KL)
--- calculate_KL/calculate_KL.sh
+++ calculate_KL/calculate_KL.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gw_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/notebooks/gwtc3_events_oldpriorrange/events"
-flow_path="/data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/flow_models/mixedmodels_0924/"
+gw_path="/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/inputs/GWTC-3/events"
+flow_path="/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/inputs/flow_models/mixed_models/"
 
 /data/wiay/2297403c/conda_envs/amaze/bin/python calculate_KL.py \
     --flow-path ${flow_path} \
     --channel-label 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
     --gw-path ${gw_path} \
-    --no-samps 100
+    --no-samps 10000
